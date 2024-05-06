dcdb1cd8cba77ff136cccc324f8d9ae31dde6f66
Nishijima <yokobase@gmail.com>
2024-05-07 01:17:55 +0900

    modified

--- examples/extras/badam/sft_test.sh
+++ examples/extras/badam/sft_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#!/bin/bash
 
 python ../../../src/train.py \
     --stage sft \
@@ -11,29 +12,29 @@ python ../../../src/train.py \
     --use_badam \
     --badam_switch_mode descending \
     --badam_switch_interval 50 \
-    --output_dir ../../../sft \
+    --output_dir /content/drive/MyDrive/GENIAC/llama3_8B \
     --overwrite_cache \
     --overwrite_output_dir \
-    --cutoff_len 1024 \
+    --cutoff_len 1536 \
     --preprocessing_num_workers 16 \
     --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
     --warmup_steps 20 \
-    --save_steps 100 \
-    --eval_steps 100 \
-    --evaluation_strategy steps \
-    --load_best_model_at_end \
+    --save_steps 5000 \
+    --max_samples 5000 \
     --learning_rate 5e-5 \
     --num_train_epochs 1.0 \
     --val_size 0.1 \
     --plot_loss \
-    --pure_bf16 \
+    --badam_verbose 2 \
+    --flash_attn fa2 \
+    --gradient_checkpointing \
     --export_hub_model_id Yasusan/llama3-ja-sft-badam    ## the Hugging Face hub ID to upload model
 
 #max_samples #For debugging purposes, truncate the number of examples for each dataset
 #optim paged_adamw_8bit \
 #--template llama2_ja \
 #--badam_verbose 2 \
+#--pure_bf16 \
