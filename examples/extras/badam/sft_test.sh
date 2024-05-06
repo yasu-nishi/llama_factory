575edb87a5dea7fc7b22fb44be8992c6a6dda987
Nishijima <yokobase@gmail.com>
2024-05-06 16:55:51 +0900

    modified

--- examples/extras/badam/sft_test.sh
+++ examples/extras/badam/sft_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 llamafactory-cli train \
+python ../../../train.py \
     --stage sft \
     --do_train \
-    --model_name_or_path meta-llama/Llama-2-7b-hf \
-    --dataset oasst_21k_ja \
+    --model_name_or_path kawagoshi-llm-team/12B_step2000 \
+    --dataset oasst2_33k_ja,oasst1_21k_ja,ichikara,oasst2_33k_en,dolly_ja \
     --dataset_dir ../../../data \
     --template llama2_ja \
     --finetuning_type full \
@@ -15,7 +15,7 @@ CUDA_VISIBLE_DEVICES=0 llamafactory-cli train \
     --output_dir ../../../sft \
     --overwrite_cache \
     --overwrite_output_dir \
-    --cutoff_len 1024 \
+    --cutoff_len 2048 \
     --preprocessing_num_workers 16 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
@@ -28,8 +28,11 @@ CUDA_VISIBLE_DEVICES=0 llamafactory-cli train \
     --evaluation_strategy steps \
     --load_best_model_at_end \
     --learning_rate 5e-5 \
-    --num_train_epochs 3.0 \
-    --max_samples 3000 \
+    --num_train_epochs 1.0 \
+    --max_samples 100 \
     --val_size 0.1 \
     --plot_loss \
-    --pure_bf16
+    --pure_bf16 \
+    --export_hub_model_id Yasusan/llama3-ja-sft-badam    ## the Hugging Face hub ID to upload model
+
+#max_samples #For debugging purposes, truncate the number of examples for each dataset
